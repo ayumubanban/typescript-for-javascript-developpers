abeeb985603aac69b1ca6137d2ecd53ba05fc3aa
ayumubanban <ayumuban@outlook.com>
2020-08-29 19:15:07 +0900

    :sparkles: Study overload

---
+++ src/overloads.ts
@@ -0,0 +1,23 @@
+export {};
+
+// signature シグネチャー
+function double(value: number): number;
+function double(value: string): string;
+
+function double(value: any): any {
+  console.log(typeof value);
+  if (typeof value === 'number') {
+    return value * 2;
+  } else {
+    // signatureで定義してるので、これはstring型しか返されないよね
+    return value + value;
+  }
+}
+
+// function double(value: string): string {
+//   return value + value;
+// }
+
+console.log(double(100));
+console.log(double('Go '));
+// console.log(double(true));
